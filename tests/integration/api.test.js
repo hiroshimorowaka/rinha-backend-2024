f69b5661688daf69359f51cf85696eea6d09561e
hiroshimorowaka <guilhermecabral1204@gmail.com>
2024-02-16 22:19:47 -0300

    update tests

--- tests/integration/api.test.js
+++ tests/integration/api.test.js
@@ -2,38 +2,46 @@ import { expect, test } from "bun:test";
 
 test("POST TO /clientes/1/transacoes: value: 200, type: D should return -200", async () => {
 	const requestBody = {
-		valor: 200,
+		valor: 1,
 		tipo: "d",
 		descricao: "teste",
 	};
-	const response = await fetch("http://localhost:8080/clientes/1/transacoes", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(requestBody),
-	});
+	let response;
+	for (let i = 0; i < 25; i++) {
+		response = await fetch("http://localhost:9999/clientes/1/transacoes", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(requestBody),
+		});
+	}
+
 	const { limite, saldo } = await response.json();
 
 	expect(response.status).toBe(200);
 
-	expect(saldo).toBe(-200);
+	expect(saldo).toBe(-25);
 	expect(limite).toBe(100000);
 });
 
 test("POST TO /clientes/1/transacoes: value: 200, type: C should return 0", async () => {
 	const requestBody = {
-		valor: 200,
+		valor: 1,
 		tipo: "c",
 		descricao: "teste",
 	};
-	const response = await fetch("http://localhost:8080/clientes/1/transacoes", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(requestBody),
-	});
+	let response;
+	for (let i = 0; i < 25; i++) {
+		response = await fetch("http://localhost:9999/clientes/1/transacoes", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(requestBody),
+		});
+	}
+
 	const { limite, saldo } = await response.json();
 
 	expect(response.status).toBe(200);
@@ -43,12 +51,7 @@ test("POST TO /clientes/1/transacoes: value: 200, type: C should return 0", asyn
 });
 
 test("GET TO /clientes/1/extrato should return total = 0 and 2 transactions", async () => {
-	const requestBody = {
-		valor: 200,
-		tipo: "c",
-		descricao: "teste",
-	};
-	const response = await fetch("http://localhost:8080/clientes/1/extrato");
+	const response = await fetch("http://localhost:9999/clientes/1/extrato");
 	const { saldo, ultimas_transacoes } = await response.json();
 
 	expect(response.status).toBe(200);
@@ -56,7 +59,12 @@ test("GET TO /clientes/1/extrato should return total = 0 and 2 transactions", as
 	expect(saldo.total).toBe(0);
 	expect(saldo.limite).toBe(100000);
 
-	expect(ultimas_transacoes.length).toBe(2);
-	expect(ultimas_transacoes[0].tipo).toBe("c");
-	expect(ultimas_transacoes[1].tipo).toBe("d");
+	expect(ultimas_transacoes.length).toBe(10);
+
+	const isAllCredit = ultimas_transacoes.every((item) => item.tipo === "c");
+	expect(isAllCredit).toBe(true);
+});
+
+test("Delete tests insertions from database", async () => {
+	await fetch("http://localhost:9999/deletedb");
 });
