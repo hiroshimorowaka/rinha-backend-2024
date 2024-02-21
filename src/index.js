e386d07547ae5477318657da4ff1cce75bae2e60
hiroshimorowaka <guilhermecabral1204@gmail.com>
2024-02-21 05:43:25 -0300

    feat: Add cluster

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
-import express from "express";
+import cluster from "cluster";
 
+import express from "express";
 import { deleteDatabase } from "./routes/deletedb.js";
 import { extratoRoute } from "./routes/extrato.js";
 import { transferenciaRoute } from "./routes/transferencia.js";
 
-const app = express();
-app.use(express.json());
-app.get("/", (req, res) => {
-	res.send("hello world");
-});
+const numCPUs = 3;
 
-app.post("/clientes/:id/transacoes", transferenciaRoute);
-app.get("/clientes/:id/extrato", extratoRoute);
-app.get("/deletedb", deleteDatabase);
+if (cluster.isPrimary) {
+	console.log(`Master process ${process.pid} is running`);
 
-app.listen(process.env.HTTP_PORT || 8080, () => {
-	console.log("server listening");
-});
+	for (let i = 0; i < numCPUs; i++) {
+		cluster.fork();
+	}
+
+	cluster.on("exit", (worker) => {
+		console.log(`Worker process ${worker.process.pid} died. Restarting...`);
+		cluster.fork();
+	});
+} else {
+	const app = express();
+	app.use(express.json());
+	app.get("/", (_, res) => {
+		res.send("hello world");
+	});
+
+	app.post("/clientes/:id/transacoes", transferenciaRoute);
+	app.get("/clientes/:id/extrato", extratoRoute);
+	app.get("/deletedb", deleteDatabase);
+
+	app.listen(process.env.HTTP_PORT || 8080, () => {
+		console.log("server listening");
+	});
+}
